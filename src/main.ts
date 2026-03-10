cb02455d2fc28f006c1c7a53cb2f053d587b9e36
xScherpschutter <konradlisthrop@gmail.com>
2026-03-09 19:25:53 -0500

    chore(frontend): setup main entry point and global styles

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
+import "./style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import es from "./locales/es.json";
+import en from "./locales/en.json";
+
+const i18n = createI18n({
+  legacy: false,
+  locale: "en",
+  fallbackLocale: "es",
+  messages: { es, en },
+});
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.use(i18n);
+app.mount("#app");
