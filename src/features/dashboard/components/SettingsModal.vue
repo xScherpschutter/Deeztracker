d9aae6091326e4e500a631908f7c7d4dd5ea710c
xScherpschutter <konradlisthrop@gmail.com>
2026-03-28 22:22:47 -0500

    edit(settings): edit version label

--- src/features/dashboard/components/SettingsModal.vue
+++ src/features/dashboard/components/SettingsModal.vue
@@ -263,7 +263,7 @@ const handleLogout = () => {
           <section class="pt-2">
             <div class="flex items-center justify-between text-xs text-textGray">
               <span>Deeztracker</span>
-              <span>{{ t('settings.about.version') }} 0.1.0</span>
+              <span>{{ t('settings.about.version') }} 0.1.1</span>
             </div>
           </section>
 
